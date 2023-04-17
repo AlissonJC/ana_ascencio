3a5ed3e0afda473f16df2c4be74bd6590c2bb12a
alissonjc <alisson.cavalcante94@gmail.com>
2023-04-17 16:36:24 -0300

    Problems 001 and 002

---
+++ cap3/propostos/001.py
@@ -0,0 +1,5 @@
+number_1, number_2 = map(float, input("Digite dois números: ").split())
+
+result = number_1 - number_2
+
+print(f"O resultado é: {result:.2f}")
---
+++ cap3/propostos/002.py
@@ -0,0 +1,5 @@
+number_1, number_2, number_3 = map(float, input("Digite três números: ").split())
+
+result = number_1 * number_2 * number_3
+
+print(f"O resultado é: {result:.2f}")
